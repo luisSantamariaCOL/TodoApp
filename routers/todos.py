75ceef47944a4a9e6d3dab1c165ceb58057a6a36
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-12-03 08:26:22 -0500

    edit-todo template created

--- routers/todos.py
+++ routers/todos.py
@@ -39,7 +39,7 @@ class Todo(BaseModel):
 
 @router.get("/test")
 async def test(request: Request):
-    return templates.TemplateResponse("add-todo.html", {"request": request})
+    return templates.TemplateResponse("edit-todo.html", {"request": request})
 
 @router.get("/")
 async def read_all(db: Session = Depends(get_db)):
