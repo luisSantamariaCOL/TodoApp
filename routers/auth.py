f8ad75ff6bcdfc777101ed7b356476545aca6b5a
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-12-03 20:54:22 -0500

    login and register templates

--- routers/auth.py
+++ routers/auth.py
@@ -1,21 +1,24 @@
+import models
+
 import sys
 sys.path.append("..")
 
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, Request, status, APIRouter
 from pydantic import BaseModel
 from typing import Optional
-import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
 ALGORITHM = "HS256"
 
+templates = Jinja2Templates(directory="templates")
 
 class CreateUser(BaseModel):
     username: str
@@ -120,6 +123,14 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
                                 expires_delta=token_expires)
     return {"token": token}
 
+
+@router.get("/", response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.get("/register", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 #Exceptions
 def get_user_exception():
