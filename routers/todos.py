90e07d35d6ea6a72cf22f958b7d17454f6028a75
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-12-04 11:11:18 -0500

    add-todo functionality implemented

--- routers/todos.py
+++ routers/todos.py
@@ -1,15 +1,24 @@
 import sys
 sys.path.append("..")
 
-from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
 import models
+from fastapi import (
+    Depends, 
+    HTTPException, 
+    APIRouter, 
+    Request, 
+    Form
+    )
+
+from starlette.status import (HTTP_302_FOUND)
+
+from typing import Optional
+from models import Todos
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from .auth import get_current_user, get_user_exception
-
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
@@ -32,13 +41,34 @@ def get_db():
 
 
 @router.get("/", response_class=HTMLResponse)
-async def read_all_by_user(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
+    
+    todos = db.query(Todos).filter(Todos.owner_id == 1).all()
+    
+    return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, 
+                      title: str = Form(...),
+                      description: str = Form(...), 
+                      priority: int = Form(...),
+                      db: Session = Depends(get_db)):
+    todo_model = Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1 # tmp
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
 async def edit_todo(request: Request):
