3d5ba688819a6a566ed072f33e7346b0868943dc
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-12-05 16:11:40 -0500

    edit and complete functionalities

--- routers/todos.py
+++ routers/todos.py
@@ -71,6 +71,50 @@ async def create_todo(request: Request,
     return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
-async def edit_todo(request: Request):
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo})
 
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(request: Request, todo_id: int, 
+                           title: str = Form(...), 
+                           description: str = Form(...),
+                           priority: int = Form(...),
+                           db: Session = Depends(get_db)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    
+    db.add(todo_model)
+    db.commit()
+    
+    return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request: Request, todo_id: int,
+                      db: Session = Depends(get_db)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id)\
+        .filter(Todos.owner_id == 1).first()
+    
+    if todo_model is None:
+        return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
+    
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    
+    db.commit()
+    
+    return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
+
+@router.get("/complete/{todo_id}", response_class=HTMLResponse)
+async def complete_todo(request: Request, todo_id: int, 
+                        db: Session = Depends(get_db)):
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+    
+    todo.complete = not todo.complete
+    
+    db.add(todo)
+    db.commit()
+    
+    return RedirectResponse(url="/todos", status_code=HTTP_302_FOUND)
